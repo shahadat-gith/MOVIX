468012b23d5087471b1fbb1613225bbac3a221b0
ShahadatGithub <shahadat.offl@gmail.com>
2026-06-26 00:45:46 +0530

    added hf api for prod

--- server/src/services/ai.service.js
+++ server/src/services/ai.service.js
@@ -1,10 +1,29 @@
 import axios from "axios";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import { InferenceClient } from "@huggingface/inference";
 
-dotenv.config()
+dotenv.config();
+
+const environment = process.env.NODE_ENV;
+
+const hfClient = new InferenceClient(process.env.HF_TOKEN);
 
 export const generateEmbedding = async (query) => {
-  const response = await axios.post(`${process.env.AI_SERVICE_URL}/api/embedding`,{ query });
+  if (environment === "production") {
+    const embedding = await hfClient.featureExtraction({
+      model: "sentence-transformers/all-MiniLM-L6-v2",
+      inputs: query,
+    });
+
+    return embedding;
+  }
+
+  const response = await axios.post(
+    `${process.env.AI_SERVICE_URL}/api/embedding`,
+    {
+      query,
+    }
+  );
 
   return response.data.embedding;
 };
